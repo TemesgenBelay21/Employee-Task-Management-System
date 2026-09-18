f1168960d241308aa1cfe94ae2da78d28006f465
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-09-18 20:19:59 +0100

    fix: load helper functions in task save controller to fix undefined redirect()

--- app/controllers/task_save.php
+++ app/controllers/task_save.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../config/db.php';
 require_once __DIR__ . '/../includes/session.php';
+require_once __DIR__ . '/../includes/functions.php';
 requireAuth();
 
 if (!isset($_POST['task_id'])) {
