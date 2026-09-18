93f23b93f0ff07bd628177507e2062076ba51ddc
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-09-18 20:20:00 +0100

    fix: load helper functions in delete user controller to fix undefined redirect()

--- app/controllers/delete_user.php
+++ app/controllers/delete_user.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../config/db.php';
 require_once __DIR__ . '/../includes/session.php';
+require_once __DIR__ . '/../includes/functions.php';
 requireAuth('admin');
 
 $id = (int)($_GET['id'] ?? 0);
